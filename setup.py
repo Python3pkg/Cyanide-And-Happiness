f7f15204a87136d8d1571266709f26f68a64f952
Stephen Kang <jkkang@sfu.ca>
2016-02-09 02:41:39 -0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup
 
 setup(name="Cyanide-And-Happiness",
-      version="1.0",
+      version="1.21",
       description="Automatically sets the latest Cyanide and Happiness comic as your background",
       url="https://github.com/sacert/Cyanide-And-Happiness",
       author="Stephen Kang",
       author_email="stephenkang9@gmail.com",
       packages=["cynhap"],
       install_requires=[
-      'beautifulsoup4'
+      'beautifulsoup4',
       'pillow',
-      'requests',
+      'requests'
       ],
       scripts=["src/cynhap"]
 )
